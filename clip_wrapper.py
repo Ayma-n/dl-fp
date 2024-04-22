3eb748c774f3348a4374bf047f6bc69cd2cc434d
Ayma-n <aymanbt14@gmail.com>
2024-04-22 01:44:44 -0400

    Add batch_get_image_encodings method

--- clip_wrapper.py
+++ clip_wrapper.py
@@ -1,11 +1,12 @@
 import torch
 import clip
 from PIL import Image
+import tensorflow as tf
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-def get_image_encoding(filepath: str):
+def get_image_encoding_from_filepath(filepath: str):
     image = preprocess(Image.open(filepath)).unsqueeze(0).to(device)
     with torch.no_grad():
         image_features = model.encode_image(image)
@@ -16,6 +17,11 @@ def get_text_encoding(tokens: list[str]):
     with torch.no_grad():
         text_features = model.encode_text(text)
         return text_features
+    
+def batch_get_image_encodings(images: tf.Tensor):
+    with torch.no_grad():
+        image_features = model.encore_image(images)
+        return image_features.numpy()
 
 ### These might be on the notebook? ###
 # 1. String with literal description of what we want to generate
