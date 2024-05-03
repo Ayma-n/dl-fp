771c385f3410ed6727869764839a632791ae1d09
arianaazarbal <arianaazarbal@icloud.com>
2024-05-03 05:03:10 -0400

    misspelled alpaca

--- offline_preprocess.py
+++ offline_preprocess.py
@@ -5,7 +5,7 @@ from pycocotools.coco import COCO
 import shutil
 
 buzzwords = ["cow", "sheep", "mountain", "hill", "countryside", "grass", "forest", "nature", 
-                 "farm", "alpacca", "horse", "landscape", "fence"]
+                 "farm", "alpaca", "horse", "landscape", "fence"]
 
 def check_naturey(captions: list[str]):
     #returns true if a caption for this image has one of our buzz words, false otherwise
