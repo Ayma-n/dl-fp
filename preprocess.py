e31cd58a36cd656ab3a0880a98475bd8d0724d6a
arianaazarbal <arianaazarbal@icloud.com>
2024-05-03 22:28:11 -0400

    expanded preprocess

--- preprocess.py
+++ preprocess.py
@@ -122,13 +122,36 @@ def get_64x64_images(dataset):
 
 def get_64x64_images_and_embeddings(dataset):
     def resize(image, clip_im_embed, captions, clip_txt_embeds, tokens):
+        return tf.image.resize(image, [64, 64]), clip_im_embed
+    return dataset.map(resize)
+
+def get_128x128_images_and_embeddings(dataset):
+    def resize(image, clip_im_embed, captions, clip_txt_embeds, tokens):
         return tf.image.resize(image, [128, 128]), clip_im_embed
     return dataset.map(resize)
 
-def get_64x64_images_and_text_embeddings(dataset):
+def get_64x64_images_and_5_text_embeddings(dataset):
+    def create_miniset(image, clip_im_embeds, captions, clip_txt_embeds, tokens):
+        miniset = tf.data.Dataset.from_tensor_slices(clip_txt_embeds)
+        def populate(clip_txt_embed):
+            return tf.image.resize(image, [64, 64]), tf.expand_dims(clip_txt_embed, axis=0)
+        return miniset.map(populate)
+    return dataset.flat_map(create_miniset)
+
+def get_64x64_images_and_1_text_embedding(dataset):
+    def populate(image, clip_im_embeds, captions, clip_txt_embeds, tokens):
+        return tf.image.resize(image, [64, 64]), clip_txt_embeds[0]
+    return dataset.map(populate)
+
+def get_128x128_images_and_5_text_embeddings(dataset):
     def create_miniset(image, clip_im_embeds, captions, clip_txt_embeds, tokens):
         miniset = tf.data.Dataset.from_tensor_slices(clip_txt_embeds)
         def populate(clip_txt_embed):
             return tf.image.resize(image, [128, 128]), tf.expand_dims(clip_txt_embed, axis=0)
         return miniset.map(populate)
     return dataset.flat_map(create_miniset)
+
+def get_64x64_images_and_1_text_embedding(dataset):
+    def populate(image, clip_im_embeds, captions, clip_txt_embeds, tokens):
+        return tf.image.resize(image, [128, 128]), clip_txt_embeds[0]
+    return dataset.map(populate)
